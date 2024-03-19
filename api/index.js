0b131db0eaa52e017953d33dd9453ff02067c2e2
shubhambartwal <shubhambartwal58@gmail.com>
2024-03-19 21:54:10 +0530

    login started registration completed

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const User = require("./models/User");
 const mongoose = require("mongoose");
+const bcrypt =require('bcryptjs')
 const app = express();
+const salt=bcrypt.genSaltSync(10);
 app.use(cors());
 app.use(express.json());
 mongoose.connect(
@@ -13,11 +15,21 @@ mongoose.connect(
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   try {
-    const userDoc = await User.create({ username, password });
+    const userDoc = await User.create({ username,
+         password:bcrypt.hashSync(password,salt) });
     res.json(userDoc);
   } catch (error) {
     res.status(400).json(error)
   }
 
 });
+app.post("/login",async (req,res)=>{
+    const {username,password}=req.body
+    const userDoc=await User.findOne({username})
+    try {
+        
+    } catch (error) {
+        
+    }
+})
 app.listen(4000,()=>console.log("Server is running at port 4000"));
--- client/blog/src/components/Pages/LoginPage.jsx
+++ client/blog/src/components/Pages/LoginPage.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+const [username,setUsername]=useState('')
+const [password,setPassword]=useState('')
+const handleSubmit=async(e)=>{
+e.preventDefault();
+try {
+  await fetch("http://localhost:4000/login",{
+    method:'POST',
+    body:JSON.stringify({username,password}),
+    headers:{'content-Type':'application/json'}
+  })
+} catch (error) {
+  console.log(error)  
+}
+}
 function LoginPage() {
   return (
-   <form className='login'>
+   <form onSubmit={handleSubmit} className='login'>
     <h1>Login</h1>
-<input type="text" placeholder="username"/>
-<input type ='password' placeholder="password"/>
+<input type="text" placeholder="username" value={username}
+onChange={(e)=>setUsername(e.target.value)}/>
+<input type ='password' placeholder="password"
+ value ={password}
+ onChange={(e)=>setPassword(e.target.value)}/>
 <button>Login</button>
    </form>
   )
