c234e228418f289836482143fac927a8b1bd7b2b
shubhambartwal <shubhambartwal58@gmail.com>
2024-03-20 16:16:14 +0530

    login and registration finished

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ const cors = require("cors");
 const User = require("./models/User");
 const mongoose = require("mongoose");
 const bcrypt =require('bcryptjs')
+const jwt=require('jsonwebtoken')
 const app = express();
 const salt=bcrypt.genSaltSync(10);
-app.use(cors());
+const secret='asdfadfs121@'
+app.use(cors({credentials:true,origin:'http://127.0.0.1:5173'}));
 app.use(express.json());
 mongoose.connect(
   "mongodb+srv://blog:ApqD3UCwOiUUK8uI@cluster0.3oorfez.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -25,11 +27,22 @@ app.post("/register", async (req, res) => {
 });
 app.post("/login",async (req,res)=>{
     const {username,password}=req.body
-    const userDoc=await User.findOne({username})
-    try {
-        
-    } catch (error) {
-        
+     try {
+        const userDoc=await User.findOne({username})
+        const passOk= bcrypt.compareSync(password,userDoc.password)
+//    res.json(passOk)      
+if(passOk){
+    //login user
+jwt.sign({username,id:userDoc._id},secret,{},(err,token)=>{
+if(err)throw err;
+res.cookie('token',token).json('ok')
+})
+}   
+else{
+    res.status(400).json('wrong credentials')
+}
+} catch (error) {
+      res.json(error)  
     }
 })
 app.listen(4000,()=>console.log("Server is running at port 4000"));
--- client/blog/src/components/Pages/LoginPage.jsx
+++ client/blog/src/components/Pages/LoginPage.jsx
@@ -1,19 +1,30 @@
 import React, { useState } from 'react'
-const [username,setUsername]=useState('')
+import  {Navigate} from 'react-router-dom'
+function LoginPage() {
+  const [username,setUsername]=useState('')
 const [password,setPassword]=useState('')
+const [redirect,setRedirect]=useState(false)
 const handleSubmit=async(e)=>{
 e.preventDefault();
 try {
-  await fetch("http://localhost:4000/login",{
+ const response= await fetch("http://localhost:4000/login",{
     method:'POST',
     body:JSON.stringify({username,password}),
-    headers:{'content-Type':'application/json'}
+    headers:{'content-Type':'application/json'},
+    credentials:'include'
   })
+  if(response.ok){
+    setRedirect(true)
+  }
+  else{
+    alert('Wrong credentials')
+  }
 } catch (error) {
   console.log(error)  
+}}
+if(redirect){
+  return <Navigate to={'/'}/>
 }
-}
-function LoginPage() {
   return (
    <form onSubmit={handleSubmit} className='login'>
     <h1>Login</h1>
